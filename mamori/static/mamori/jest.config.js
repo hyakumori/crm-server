7862d8660d3accc1a6fa9c416dd82f685f83b010
Tran Viet Ha <hatv@hybrid-technologies.co.jp>
2020-03-27 04:09:21 +0000

    Setup ci

---
+++ mamori/static/mamori/jest.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  moduleFileExtensions: ["js", "json", "vue"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1"
+  },
+  moduleDirectories: ["node_modules", "src"],
+  transform: {
+    "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
+    ".*\\.(vue)$": "<rootDir>/node_modules/vue-jest"
+  },
+  snapshotSerializers: ["<rootDir>/node_modules/jest-serializer-vue"],
+  setupFiles: ["<rootDir>/tests/setup"],
+  coverageDirectory: "<rootDir>/tests/coverage",
+  collectCoverageFrom: [
+    "src/**/*.{js,vue}",
+    "!src/main.js",
+    "!src/routers.js",
+    "!**/node_modules/**"
+  ]
+};
