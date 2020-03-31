23aa3d05545ad196094a4d1569021868bbd8ced9
Tran Viet Ha <hatv@hybrid-technologies.co.jp>
2020-03-31 09:14:15 +0000

    #67 graphql common

--- hyakumori_crm/core/models.py
+++ hyakumori_crm/core/models.py
@@ -1,3 +1,5 @@
+from typing import Any
+import datetime
 from uuid import UUID
 from django.db import models
 from pydantic import BaseModel, validator
@@ -12,7 +14,7 @@ class TimestampMixin(models.Model):
 
 
 class InternalMixin(models.Model):
-    internal_id = models.CharField(max_length=255)
+    internal_id = models.CharField(max_length=255, null=True)
 
     class Meta:
         abstract = True
@@ -28,3 +30,24 @@ class HyakumoriDanticModel(BaseModel):
         if isinstance(v, UUID):
             return str(v)
         return v
+
+    @validator("attributes", pre=True, always=True, check_fields=False)
+    def validate_attributes(cls, v):
+        if v is None:
+            return {}
+        return v
+
+
+class HyakumoriDanticUpdateModel(HyakumoriDanticModel):
+    context: Any
+    updated_at: datetime.datetime
+
+    @validator("updated_at")
+    def validate_updated_at(cls, v, values):
+        """
+        Check if data is newer than the one from database,
+        if not, we have to warn the user.
+        """
+        latest_update = values["context"]["updated_at"]
+        if v < latest_update:
+            raise ValueError("Some data has changed")
