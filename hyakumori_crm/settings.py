98f5570a4a3a3447e78ec74ef70a6bd6069048b3
Team Hyakumori <info@hyakumori.com>
2020-04-14 14:14:38 +0700

    added rest api for customer detail, forest detail, user listing, basic permission

--- hyakumori_crm/settings.py
+++ hyakumori_crm/settings.py
@@ -203,7 +203,7 @@ DJOSER = {
 
 # Simple JWT
 SIMPLE_JWT = {
-    "AUTH_HEADER_TYPES": ("JWT",),
+    "AUTH_HEADER_TYPES": ("JWT", "Bearer"),
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": False,
