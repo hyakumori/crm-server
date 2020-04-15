8a8966ce62c3d22c030b934b404bc8b506251659
Nguyen Anh Tu <tuna@hybrid-technologies.co.jp>
2020-04-15 05:37:38 +0000

    Added user permission admin apis

--- hyakumori_crm/crm/models/relations.py
+++ hyakumori_crm/crm/models/relations.py
@@ -13,6 +13,9 @@ class CustomerContact(BaseRelationModel):
         default=False
     )  # if True, will show in the list of Owners for select direct owners
 
+    class Meta:
+        permissions = [("manage_customercontact", "All permissions for customer contact"), ]
+
     @property
     def is_default(self):
         return self.attributes["is_default"]
@@ -34,13 +37,22 @@ class ForestCustomer(BaseRelationModel):
     customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
     contact = models.ForeignKey(Contact, on_delete=models.DO_NOTHING)
 
+    class Meta:
+        permissions = [("manage_forestcustomer", "All permissions for forest customer"), ]
+
 
 class ArchiveForest(BaseRelationModel):
     archive = models.ForeignKey(Archive, on_delete=models.DO_NOTHING)
     forest = models.ForeignKey(Forest, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        permissions = [("manage_archivecustomer", "All permissions for archive forest"), ]
 
 
 class ArchiveCustomer(BaseRelationModel):
     archive = models.ForeignKey(Archive, on_delete=models.DO_NOTHING)
     customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
     contact = models.ForeignKey(Contact, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        permissions = [("manage_archivecustomer", "All permissions for archive customer"), ]
