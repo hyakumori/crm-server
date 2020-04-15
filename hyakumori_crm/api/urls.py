da11fe86f93dae1f197b911936803c25a730bccd
Team Hyakumori <info@hyakumori.com>
2020-04-15 02:14:34 +0000

    Api module

---
+++ hyakumori_crm/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import re_path
+from hyakumori_crm.forest.urls import api_urls as forest_api_urls
+
+from hyakumori_crm.customer.urls import api_urls as customer_api_urls
+from hyakumori_crm.users.urls import api_urls as user_api_urls
+from hyakumori_crm.permissions.urls import api_urls as permission_api_urls
+from .views import notfound_view
+
+urlpatterns = (
+    forest_api_urls
+    + customer_api_urls
+    + user_api_urls
+    + permission_api_urls
+    + [re_path(".*", notfound_view)]
+)
--- hyakumori_crm/users/urls.py
+++ hyakumori_crm/users/urls.py
@@ -1,16 +1,16 @@
 from django.urls import include, path
 from djoser.views import UserViewSet, TokenDestroyView
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 from .views import TokenObtainPairView
 
-router = DefaultRouter()
+router = SimpleRouter(trailing_slash=False)
 router.register("users", UserViewSet, basename="user")
 
-urlpatterns = [
+api_urls = router.urls
+api_urls += [
     path("token/create/", TokenObtainPairView.as_view(), name="jwt-create"),
     path("token/refresh/", TokenRefreshView.as_view(), name="jwt-refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="jwt-verify"),
-    path("", include(router.urls))
 ]
