5f8850304893191de6a356cd18a2a12a0efa1e68
Team Hyakumori <info@hyakumori.com>
2020-04-16 03:57:51 +0000

    Sprint 3 fe customer detail

---
+++ hyakumori_crm/static/hyakumori_crm/src/components/OutlineRoundBtn.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-btn rounded dark outlined v-on="on">
+    <v-icon>{{ icon }}</v-icon>
+    {{ content }}
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: "outline-round-btn",
+
+  props: {
+    on: Object,
+    icon: String,
+    content: String,
+  },
+};
+</script>
