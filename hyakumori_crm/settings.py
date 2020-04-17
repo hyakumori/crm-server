49aa716198a972b2027240cd473a1f9358a3ef56
Team Hyakumori <info@hyakumori.com>
2020-04-17 16:15:54 +0700

    Extend token lifetime and fix list wrong columns

--- hyakumori_crm/settings.py
+++ hyakumori_crm/settings.py
@@ -199,7 +199,7 @@ DJOSER = {
 # Simple JWT
 SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("JWT", "Bearer"),
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": True,
