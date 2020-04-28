038adb1fa4b7794678ebb477d241294a820c7a49
Huynh Phuoc Bao <baohp@hybrid-technologies.vn>
2020-04-28 04:38:14 +0000

    Sprint 4 impl archive api

--- hyakumori_crm/crm/models/relations.py
+++ hyakumori_crm/crm/models/relations.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 from ...core.models import BaseRelationModel
@@ -68,4 +69,4 @@ class ArchiveCustomerContact(BaseRelationModel):
 
 class ArchiveUser(BaseRelationModel):
     archive = models.ForeignKey("Archive", on_delete=models.PROTECT)
-    user = models.ForeignKey("users.User", on_delete=models.PROTECT)
+    user = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
