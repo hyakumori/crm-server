4ea6c83685922a2a57a827f656e3b53c6c3c3d98
Nguyen Anh Tu <tuna@hybrid-technologies.co.jp>
2020-04-30 05:47:10 +0000

    Permission rest apply

--- hyakumori_crm/permissions/policies.py
+++ hyakumori_crm/permissions/policies.py
@@ -12,11 +12,6 @@ def can_view_customers(request, user):
            or user.member_of(SystemGroups.GROUP_NORMAL_USER)
 
 
-def can_create_customers(request, user):
-    return user.member_of(SystemGroups.GROUP_ADMIN) \
-           or user.member_of(SystemGroups.GROUP_NORMAL_USER)
-
-
 def can_view_archives(request, user):
     return user.member_of(SystemGroups.GROUP_ADMIN) \
            or user.member_of(SystemGroups.GROUP_NORMAL_USER)
