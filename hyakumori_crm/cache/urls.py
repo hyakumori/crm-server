60292934503e678e7b0b2ea2ad70f1af889a9807
Team Hyakumori <info@hyakumori.com>
2020-06-09 17:51:55 +0700

    fix search or in archivelist

--- hyakumori_crm/cache/urls.py
+++ hyakumori_crm/cache/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from .restful import *
+from .restful import reload_single_archive_cache, reload_forest_cache
 
 api_urls = [
-    path("cache/archives/<uuid:pk>", view=reload_single_archive_cache, name="reload-single-archive-cache"),
     path(
-        "cache/forests",
-        view=reload_forest_cache,
-        name="reload-forest-cache",
+        "cache/archives/<uuid:pk>",
+        view=reload_single_archive_cache,
+        name="reload-single-archive-cache",
     ),
+    path("cache/forests", view=reload_forest_cache, name="reload-forest-cache",),
 ]
