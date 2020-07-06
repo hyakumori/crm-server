3fc252274c69ff92198422cbc749bd52bfd64201
Team Hyakumori <info@hyakumori.com>
2020-07-06 18:06:40 +0700

    update customer csv following new CR

--- hyakumori_crm/static/hyakumori_crm/src/helpers/forest.js
+++ hyakumori_crm/static/hyakumori_crm/src/helpers/forest.js
@@ -1,11 +1,8 @@
 export function getForestDisplayName(forest) {
-  const sector = forest.cadastral.sector ? forest.cadastral.sector + "_" : "";
-  const subsector = forest.cadastral.subsector
-    ? forest.cadastral.subsector + "_"
-    : "";
+  const sector = forest.cadastral.sector;
   const lotnumber = forest.land_attributes["地番本番"] || "";
   const sub_lotnumber = forest.land_attributes["地番支番"]
-    ? "_" + forest.land_attributes["地番支番"]
+    ? "-" + forest.land_attributes["地番支番"]
     : "";
-  return `${sector}${subsector}${lotnumber}${sub_lotnumber}`;
+  return `${sector} ${lotnumber}${sub_lotnumber}`;
 }
