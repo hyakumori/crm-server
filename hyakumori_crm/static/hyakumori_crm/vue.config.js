6537c8adf42c6b044ba29e23fd36caf37e587731
Team Hyakumori <info@hyakumori.com>
2020-07-09 10:52:08 +0700

    remove inject envs trick

--- hyakumori_crm/static/hyakumori_crm/vue.config.js
+++ hyakumori_crm/static/hyakumori_crm/vue.config.js
@@ -3,6 +3,8 @@ const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = {
   devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    staticOptions: { index: "index-dev.html" },
     proxy: {
       "^/api": {
         target: "http://localhost:8000",
@@ -15,41 +17,50 @@ module.exports = {
         changeOrigin: true,
       },
     },
-    historyApiFallback: true,
     overlay: {
       warnings: false,
       errors: true,
     },
   },
-  configureWebpack: {
-    plugins: [
-      new CompressionPlugin({
-        filename: "[path].gz[query]",
-        algorithm: "gzip",
-        test: /\.js$|\.css$|\.html$/,
-        exclude: "index.html",
-        minRatio: 0.8,
-      }),
-      new CompressionPlugin({
-        filename: "[path].br[query]",
-        algorithm: "brotliCompress",
-        test: /\.(js|css|html|svg)$/,
-        exclude: "index.html",
-        compressionOptions: {
-          // zlib’s `level` option matches Brotli’s `BROTLI_PARAM_QUALITY` option.
-          level: 11,
-        },
-        threshold: 10240,
-        minRatio: 0.8,
-        deleteOriginalAssets: false,
-      }),
-    ],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "src"),
-      },
-      extensions: [".vue", ".js"],
-    },
+  configureWebpack: config => {
+    if (!config.plugins) config.plugins = [];
+    if (process.env.NODE_ENV === "production") {
+      config.plugins.push(
+        new CompressionPlugin({
+          filename: "[path].gz[query]",
+          algorithm: "gzip",
+          test: /\.js$|\.css$|\.html$/,
+          exclude: "index.html",
+          minRatio: 0.8,
+        }),
+        new CompressionPlugin({
+          filename: "[path].br[query]",
+          algorithm: "brotliCompress",
+          test: /\.(js|css|html|svg)$/,
+          exclude: "index.html",
+          compressionOptions: {
+            // zlib’s `level` option matches Brotli’s `BROTLI_PARAM_QUALITY` option.
+            level: 11,
+          },
+          threshold: 10240,
+          minRatio: 0.8,
+          deleteOriginalAssets: false,
+        }),
+      );
+    }
+  },
+  chainWebpack: config => {
+    if (process.env.NODE_ENV === "production") {
+      config.plugin("copy").tap(([options]) => {
+        options[0].ignore.push("index-dev.html");
+        return [options];
+      });
+    } else {
+      config.plugin("html").tap(args => {
+        args[0].template = path.join(__dirname, "public", "index-dev.html");
+        return args;
+      });
+    }
   },
   transpileDependencies: ["vuetify"],
 };
