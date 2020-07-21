d297283c2faa01df7095a5774ef2c3d5f9fd172a
Tran Viet Ha <hatv@hybrid-technologies.co.jp>
2020-07-21 13:41:13 +0700

    improve pydantic error message

--- hyakumori_crm/crm/common/regexes.py
+++ hyakumori_crm/crm/common/regexes.py
@@ -3,6 +3,9 @@ MOBILEPHONE_NUMBER = (
     r"^(?:\d{10}|\d{3}-\d{3}-\d{4}|\d{2}-\d{4}-\d{4}|\d{3}-\d{4}-\d{4})$"
 )
 TELEPHONE_NUMBER = r"^\d{2}-\d{4}-\d{4}$"
+
+CSV_TELEPHONE_NUMBER = r"^\d{2}-\d{4}-\d{4}|\d{3}-\d{3}-\d{4}|\d{10}$"
+
 POSTAL_CODE = r"^(?:\d{3}-\d{4})$"
 
 CUSTOMER_ID = r"DFFC\d{10}"
