6ff18158e7465173199998cef6c393ac56225307
Iosefa Percival <iosefa@georepublic.de>
2021-01-19 12:42:37 +0900

    pass request authorization in headers for geoserver proxy
    Signed-off-by: Iosefa Percival <iosefa@georepublic.de>

--- hyakumori_crm/urls.py
+++ hyakumori_crm/urls.py
@@ -1,21 +1,15 @@
-import os
-
 from django.urls import include, path, re_path
-from revproxy.views import ProxyView
 
+from hyakumori_crm.geoserver.views import CustomProxyView
 from hyakumori_crm.graphql import view as graphql_view
 from hyakumori_crm.static.views import index_view, test_view
 from hyakumori_crm.api.urls import view_urls
-
-geoserver_user = os.getenv("GEOSERVER_USER", "")
-geoserver_pass = os.getenv("GEOSERVER_PASS", "")
 
 urlpatterns = [
     path("", include(view_urls)),
     path("api/v1/", include("hyakumori_crm.api.urls")),
     path("check", test_view, name="check"),
     path("graphql", graphql_view, name="graphql"),
-    re_path(r'^geoserver/(?P<path>.*)$',
-            ProxyView.as_view(upstream=f'http://{geoserver_user}:{geoserver_pass}@geoserver:8080/geoserver/')),
+    re_path(r'^geoserver/(?P<path>.*)$', CustomProxyView.as_view()),
     re_path(".*", index_view, name="index"),
 ]
