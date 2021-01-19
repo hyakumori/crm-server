6883607743d843e5485d12ccf4fcb2ba9b37ac5c
Iosefa Percival <iosefa@georepublic.de>
2021-01-19 14:08:50 +0900

    add APIView permissions
    Signed-off-by: Iosefa Percival <iosefa@georepublic.de>

--- hyakumori_crm/geoserver/views.py
+++ hyakumori_crm/geoserver/views.py
@@ -1,5 +1,7 @@
 import os
 
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 from revproxy.views import ProxyView
 from base64 import b64encode
 
@@ -10,7 +12,8 @@ credentials = f"{geoserver_user}:{geoserver_pass}"
 credentials_encoded = b64encode(credentials.encode("utf-8")).decode("utf-8")
 
 
-class CustomProxyView(ProxyView):
+class CustomProxyView(APIView, ProxyView):
+    permission_classes = [IsAuthenticated]
     upstream = 'http://geoserver:8080/geoserver/'
 
     def get_request_headers(self):
