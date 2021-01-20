bb833aefa7e7b56d5d471db6428b1ef3c8f46b64
Iosefa Percival <iosefa@georepublic.de>
2021-01-20 11:02:12 +0900

    use proxyview to check permission and remove hyakumori package from test
    Signed-off-by: Iosefa Percival <iosefa@georepublic.de>

--- hyakumori_crm/geoserver/views.py
+++ hyakumori_crm/geoserver/views.py
@@ -1,7 +1,6 @@
 import os
 
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.views import APIView
+from django.core.exceptions import PermissionDenied
 from revproxy.views import ProxyView
 from base64 import b64encode
 
@@ -12,11 +11,12 @@ credentials = f"{geoserver_user}:{geoserver_pass}"
 credentials_encoded = b64encode(credentials.encode("utf-8")).decode("utf-8")
 
 
-class CustomProxyView(APIView, ProxyView):
-    permission_classes = [IsAuthenticated]
+class CustomProxyView(ProxyView):
     upstream = 'http://geoserver:8080/geoserver/'
 
     def get_request_headers(self):
+        if not hasattr(self.request, 'user') or not self.request.user.has_perm("crm.view_forest"):
+            raise PermissionDenied()
         headers = super(CustomProxyView, self).get_request_headers()
         headers["Authorization"] = f"Basic {credentials_encoded}"
         return headers
