958a4eb0e6e75c67a677c73da2ae115cea033185
Iosefa Percival <iosefa@georepublic.de>
2021-01-29 17:39:29 +0900

    minor ui improvements
    Signed-off-by: Iosefa Percival <iosefa@georepublic.de>

--- hyakumori_crm/static/hyakumori_crm/src/screens/ForestDetail.vue
+++ hyakumori_crm/static/hyakumori_crm/src/screens/ForestDetail.vue
@@ -2,6 +2,11 @@
   <main-section class="forest-detail">
     <template #section>
       <div class="forest-detail__section px-7">
+        <map-container
+          v-if="forestDetailMapInfo"
+          :forests="[forestDetailMapInfo]"
+        />
+
         <forest-basic-info-container
           headerContent="基本情報 (登記情報)"
           toggleEditBtnContent="追加・編集"
@@ -24,14 +29,6 @@
           "
           :id="id"
         />
-
-        <map-container
-          v-if="forestDetailMapInfo"
-          class="mt-12"
-          headerContent="地図"
-          :forests="[forestDetailMapInfo]"
-        >
-        </map-container>
 
         <attachment-container
           v-acl-only="['manage_archive', 'view_archive']"
