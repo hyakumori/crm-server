c4674506bb76cdda6ef30200bf78a8271bf5669a
Iosefa Percival <iosefa@georepublic.de>
2021-02-04 15:43:58 +0900

    separate serverside code from front end
    Signed-off-by: Iosefa Percival <ipercival@gmail.com>

--- hyakumori_crm/static/hyakumori_crm/src/components/detail/ArchiveDetailMixin.js
+++
@@ -1,5 +0,0 @@
-export default {
-  props: {
-    allowEdit: { type: Boolean },
-  },
-};
--- hyakumori_crm/static/hyakumori_crm/src/helpers/document.js
+++
@@ -1,7 +0,0 @@
-import i18n from "../plugins/i18n";
-
-const makeTitle = title_key => {
-  return `${i18n.t("site_name")} - ${i18n.t(title_key)}`;
-};
-
-export { makeTitle };
